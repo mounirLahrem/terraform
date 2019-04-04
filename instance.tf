1678a89f26a9e6d04fe68865d9620281863e7c11
mounirLahrem <38960649+mounirLahrem@users.noreply.github.com>
2019-04-04 14:48:40 +0200

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
-variable "AWS_ACCESS_KEY_ID" ()
-variable "AWS_SECRET_ACCESS_KEY" ()
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
 
 provider "aws" {
   acces_key = "${var.AWS_ACCESS_KEY_ID}"
