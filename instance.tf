893f56a70985513876ce1b1fbf156d024154bfbe
mounirLahrem <38960649+mounirLahrem@users.noreply.github.com>
2019-04-05 13:12:53 +0200

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -12,9 +12,24 @@ resource "aws_instance" "frontend" {
 }
 resource "aws_instance" "bdd" {
   ami           = "ami-0b500ef59d8335eee"
-  instance_type = "t2.mmicro"
+  instance_type = "t2.micro"
   key_name = "awskey"
   tags = {
     Name = "moun-test-bdd"
   }
+}
+resource "aws_s3_bucket" "terraform-state-storage-s3" {
+    bucket = "terraform-remote-state-storage-s3"
+ 
+    versioning {
+      enabled = true
+    }
+ 
+    lifecycle {
+      prevent_destroy = true
+    }
+ 
+    tags {
+      Name = "S3 Remote Terraform State Store"
+    }      
 }
