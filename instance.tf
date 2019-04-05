53415484007fdfe6353959d8ae3e6bd0bb49e2a2
mounirLahrem <38960649+mounirLahrem@users.noreply.github.com>
2019-04-05 11:16:40 +0200

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -18,3 +18,18 @@ resource "aws_instance" "bdd" {
     Name = "moun-test-bdd"
   }
 }
+resource "aws_s3_bucket" "terraform-state-storage-s3" {
+    bucket = "terraform-remote-state-storage-s3"
+ 
+    versioning {
+      enabled = true
+    }
+ 
+    lifecycle {
+      prevent_destroy = true
+    }
+ 
+    tags {
+      Name = "S3 Remote Terraform State Store- moun test "
+    }      
+}
