52cae5c1f6661d22f39e60b936bdf46fb4a9f76e
mounirLahrem <38960649+mounirLahrem@users.noreply.github.com>
2019-04-05 12:36:50 +0200

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -33,27 +33,3 @@ resource "aws_s3_bucket" "terraform-state-storage-s3" {
       Name = "S3 Remote Terraform State Store- moun test "
     }      
 }
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "terraform-state-lock-dynamo"
-  hash_key = "LockID"
-  read_capacity = 20
-  write_capacity = 20
- 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
- 
-  tags {
-    Name = "DynamoDB Terraform State Lock Table"
-  }
-}
-terraform {
- backend “s3” {
- encrypt = true
- bucket = "terraform-remote-state-storage-s3"
- dynamodb_table = "terraform-state-lock-dynamo"
- region = us-west-2
- key = terraform
- }
-}
